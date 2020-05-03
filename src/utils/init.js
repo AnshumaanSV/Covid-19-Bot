53092964d6c73948a92096a08c7c3a5e6e4365e8
mythio <mythio.2909@gmail.com>
2020-05-03 15:18:32 +0530

    chore: Generate secrets at postbuild

---
+++ src/utils/init.js
@@ -0,0 +1,18 @@
+require('dotenv')
+const fs = require('fs')
+const path = require('path')
+
+fs.writeFileSync(path.join(__dirname, `../../`) + process.env.secret_name,
+`{
+  "type": "${process.env.type}",
+  "project_id": "${process.env.project_id}",
+  "private_key_id": "${process.env.private_key_id}",
+  "private_key": "${process.env.private_key}",
+  "client_email": "${process.env.client_email}",
+  "client_id": "${process.env.client_id}",
+  "auth_uri": "${process.env.auth_uri}",
+  "token_uri": "${process.env.token_uri}",
+  "auth_provider_x509_cert_url": "${process.env.auth_provider_x509_cert_url}",
+  "client_x509_cert_url": "${process.env.client_x509_cert_url}"
+}
+`)
